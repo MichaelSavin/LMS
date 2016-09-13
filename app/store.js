7a630a463bfe6f43d7587a01e57555b779d4b75d
Evgeny Erohin <erohin.em@gmail.com>
2016-09-13 05:13:26 +0300

    Убрано хранение стора в localSorage

--- app/store.js
+++ app/store.js
@@ -1,41 +1,26 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 
-import * as storage from 'redux-storage';
-import merger from 'redux-storage-merger-immutablejs';
-import createEngine from 'redux-storage-engine-localstorage';
-
 const sagaMiddleware = createSagaMiddleware();
+const devtools = window.devToolsExtension || (() => noop => noop);
 
 const configureStore = (initialState = {}, history) => {
-  const reducer = storage.reducer(reducers, merger);
-  const engine = createEngine('key');
-  const engineMiddleware = storage.createMiddleware(engine);
-  const createStoreWithMiddleware = compose(
-    applyMiddleware(engineMiddleware, routerMiddleware(history), sagaMiddleware),
-    // window.devToolsExtension ? window.devToolsExtension({ actionsBlacklist: ['REDUX_STORAGE_LOAD', 'REDUX_STORAGE_SAVE'] }) : f => f
-  )(createStore);
-  const store = createStoreWithMiddleware(reducer);
-  const load = storage.createLoader(engine);
-  load(store)
-    .then((newState) => console.log('Loaded state:', newState))
-    .catch(() => console.log('Failed to load previous state'));
+  const store = createStore(
+    reducers,
+    fromJS(initialState),
+    compose(
+      devtools(),
+      applyMiddleware(
+        routerMiddleware(history),
+        sagaMiddleware,
+      ),
+    ),
+  );
 
-  // const store = createStore(
-  //   reducers,
-  //   fromJS(initialState),
-  //   compose(
-  //     devtools(),
-  //     applyMiddleware(
-  //       routerMiddleware(history),
-  //       sagaMiddleware,
-  //     ),
-  //   ),
-  // );
-
-  // store.runSaga = sagaMiddleware.run;
+  store.runSaga = sagaMiddleware.run;
 
   if (module.hot) {
     System.import('./reducers').then((reducerModule) => {
@@ -47,3 +32,4 @@ const configureStore = (initialState = {}, history) => {
 };
 
 export default configureStore;
+
