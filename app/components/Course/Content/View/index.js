6c7e2b1bb410671451b7264bef4d435d0316a3aa
Evgeny Erohin <erohin.em@gmail.com>
2016-09-21 17:44:34 +0300

    Показ видео и картинок в окне просмотра

--- app/components/Course/Content/View/index.js
+++ app/components/Course/Content/View/index.js
@@ -1,14 +1,35 @@
 import React, { Component, PropTypes } from 'react';
 
 import { 
-  Editor,
+  // Editor,
   EditorState,
   convertFromRaw,
   CompositeDecorator,
 } from 'draft-js';
+import createImagePlugin, {
+  // imageStyles,
+  // imageCreator,
+} from 'draft-js-image-plugin';
+import Editor from 'draft-js-plugins-editor';
+import createVideoPlugin from 'draft-js-video-plugin';
+import createEntityPropsPlugin from 'draft-js-entity-props-plugin';
 import Link, { findLinkEntities } from '../Editor/Entities/Link';
-
 import styles from './styles.css';
+
+const imageTheme = {
+  imageLoader: 'imageLoader',
+  imageWrapper: 'imageWrapper',
+  image: 'image',
+};
+
+const plugins = [
+  createImagePlugin({
+    theme: imageTheme,
+    type: 'atomic',
+  }),
+  createVideoPlugin(),
+  createEntityPropsPlugin(),
+];
 
 class View extends Component { // HMR
   render() {
@@ -20,6 +41,7 @@ class View extends Component { // HMR
       <div className={styles.view}>
         <div className={styles.draft}>
           <Editor
+            plugins={plugins}
             editorState={EditorState
               .createWithContent(
                 convertFromRaw(
