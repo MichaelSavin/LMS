41417d9328574fcfe125a783b698cd11b6dc62c2
Evgeny Erohin <erohin.em@gmail.com>
2016-10-04 01:06:14 +0300

    Обновлены зависимости + immutable destructuring & proptypes + antd

--- app/store.js
+++ app/store.js
@@ -1,5 +1,5 @@
 import * as storage from 'redux-storage';
-import createSagaMiddleware from 'redux-saga';
+// import createSagaMiddleware from 'redux-saga';
 import { browserHistory } from 'react-router';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware, syncHistoryWithStore } from 'react-router-redux';
@@ -13,17 +13,16 @@ import { selectLocationState } from './selectors';
 const configureStore = (reducers, router) => {
   const reducer = storage.reducer(reducers, merger);
   const engine = debounce(createEngine('LMS'), 5000);
-  const sagaMiddleware = createSagaMiddleware();
   const engineMiddleware = storage.createMiddleware(engine);
+  // const sagaMiddleware = createSagaMiddleware();
   const createStoreWithMiddleware = compose(
-    applyMiddleware(engineMiddleware, routerMiddleware(browserHistory), sagaMiddleware),
+    applyMiddleware(engineMiddleware, routerMiddleware(browserHistory)), // , sagaMiddleware),
       window.devToolsExtension
         ? window.devToolsExtension({ actionsBlacklist: ['REDUX_STORAGE_LOAD', 'REDUX_STORAGE_SAVE'] })
         : f => f,
   )(createStore);
   const store = createStoreWithMiddleware(reducer);
   const load = storage.createLoader(engine);
-  store.runSaga = sagaMiddleware.run; // eslint-disable-line fp/no-mutation
   load(store).then(() => {
     const history = syncHistoryWithStore(
       browserHistory,
@@ -32,6 +31,7 @@ const configureStore = (reducers, router) => {
     );
     router(store, history);
   });
+  // store.runSaga = sagaMiddleware.run; // eslint-disable-line fp/no-mutation
 };
 
 export default configureStore;
