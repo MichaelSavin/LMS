9d12375345ffcc31ce83ec97a63eb131500e9116
webSergik <serg47181@gmail.com>
2016-10-18 14:28:59 +0300

    select component #33 bugfixing

--- app/components/Course/Content/Entities/Inline/Select/index.js
+++ app/components/Course/Content/Entities/Inline/Select/index.js
@@ -2,20 +2,18 @@ import React, {
   PropTypes,
   Component,
 } from 'react';
-import { Select as AntSelect, Button } from 'antd';
+import { Select as AntSelect, Button as AntButton } from 'antd';
 import AntPromt from 'components/UI/Promt';
 import { isEqual, uniq } from 'lodash';
-import { List } from 'immutable';
 import { Entity } from 'draft-js';
 import styles from './styles.css';
-
-const Option = AntSelect.Option;
 
 class Select extends Component {
 
   constructor(props) {
     super(props);
     const {
+      answer,
       options,
     } = Entity
       .get(this.props.entityKey)
@@ -23,7 +21,7 @@ class Select extends Component {
       .content;
     this.state = {
       ...props.content,
-      options: List(options),
+      options,
       promt: {
         open: false,
         value: null,
@@ -56,44 +54,61 @@ class Select extends Component {
 
   modifyOptions = () => {
     const options = uniq(
-      this
-      .state
-      .promt
-      .value
-      .split(';')
+      this.state.promt.value.split(';')
     );
-    const answers = this
-      .state
-      .answers
+    const answer =
+    this.state.answer > options.length - 1
+        ? undefined
+        : this.state.answer;
     Entity.replaceData(
       this.props.entityKey, {
         content: {
           options,
-          answers,
+          answer,
         },
       }
     );
     this.setState({
       options,
-      answers,
+      answer,
       promt: {
         open: false,
       },
     });
   }
 
-  chooseAnswer(optionIndex) {
-    const newContent = {
-      ...this.state,
-      answer: optionIndex,
-    };
+  chooseAnswer = (event) => {
+
+    const {
+      value: answer,
+    } = event.target;
+    console.log('event ' + value, 'answer ' + answer);
     Entity.replaceData(
       this.props.entityKey, {
-        content: newContent,
-      }
+        content: {
+          options: this
+            .state
+            .options,
+          answer,
+        },
+      },
     );
-    this.setState({ answer: optionIndex });
+    this.setState({ answer });
   }
+  //
+  // chooseAnswer(optionIndex) {
+  //   console.log(optionIndex);
+  //   const newContent = {
+  //     ...this.state,
+  //     answer: optionIndex,
+  //   };
+  //   Entity.replaceData(
+  //     this.props.entityKey, {
+  //       content: newContent,
+  //     }
+  //   );
+  //   this.setState({ answer: optionIndex });
+  // }
 
   render() {
     const {
@@ -101,26 +116,29 @@ class Select extends Component {
       options,
       answer,
     } = this.state;
-    const AntOptions = options.map((text, index) => <Option key={index}>{text}</Option>);
+    // const AntOptions = options.map((text, index) => <AntSelect.Option key={index}>{text}</AntSelect.Option>);
     return (
       <div>
         <AntSelect
-          showSearch
-          style={{ width: 200 }}
           placeholder="Выберите вариант ответа"
-          notFoundContent=""
-          contentEditable="false"
-          onChange={event =>
-            this.chooseAnswer(
-              event.target.selectedIndex
-            )
-          }
+          filterOption={false}
+          onChange={this.chooseAnswer}
           defaultValue={answer}
           className={styles.select}
         >
-          {AntOptions}
+          {options.map((text, index) =>
+            <AntSelect.Option
+              key={index}
+            >
+             {text}
+            </AntSelect.Option>)}
         </AntSelect>
-        <Button type="ghost" shape="circle-outline" icon="edit" onClick={this.editOptions} />
+        <AntButton
+          type="ghost"
+          shape="circle-outline"
+          icon="edit"
+          onClick={this.editOptions}
+        />
         <AntPromt
           value={promt.value}
           onSave={this.modifyOptions}
