9cb3206dadf08c20734367fb413e936431f94102
Evgeny Erohin <erohin.em@gmail.com>
2016-10-20 17:53:08 +0300

    Удаление зависимостей плагинов DraftJS из окна просмотра редактора

--- app/components/Course/Content/View/index.js
+++ app/components/Course/Content/View/index.js
@@ -7,13 +7,6 @@ import {
   EditorState,
   convertFromRaw,
 } from 'draft-js';
-// import createImagePlugin, {
-  // imageStyles,
-  // imageCreator,
-// } from 'draft-js-image-plugin';
-// import Editor from 'draft-js-plugins-editor';
-// import createVideoPlugin from 'draft-js-video-plugin';
-// import createEntityPropsPlugin from 'draft-js-entity-props-plugin';
 import styles from './styles.css';
 
 import {
@@ -21,28 +14,12 @@ import {
   entitiesDecorator,
 } from '../Entities';
 
-// const imageTheme = {
-//   imageLoader: 'imageLoader',
-//   imageWrapper: 'imageWrapper',
-//   image: 'image',
-// };
-
-// const plugins = [
-//   createImagePlugin({
-//     theme: imageTheme,
-//     type: 'atomic',
-//   }),
-//   createVideoPlugin(),
-//   createEntityPropsPlugin(),
-// ];
-
 class View extends Component { // HMR
   render() {
     return (
       <div className={styles.view}>
         <div className={styles.draft}>
           <Editor
-            // plugins={plugins}
             blockRendererFn={blockRenderer}
             editorState={EditorState
               .createWithContent(
