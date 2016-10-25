f0e52a9a2918fddc025bc3ff0ab78ff939a070b0
Evgeny Erohin <erohin.em@gmail.com>
2016-10-25 17:55:57 +0300

    Опция disabled для компонента Option (панель форматирования)

--- app/components/Course/Content/Editor/Toolbar/Option/index.js
+++ app/components/Course/Content/Editor/Toolbar/Option/index.js
@@ -2,20 +2,26 @@ import React, {
   Component,
   PropTypes,
 } from 'react';
-import styles from './styles.css'; // eslint-disable-line no-unused-vars
+import classNames from 'classnames';
+import styles from './styles.css';
 
 class Option extends Component {
-
   render() {
     const {
       value,
       active,
       onClick,
       children,
+      disabled,
     } = this.props;
     return (
       <div
-        className={`${styles.option} ${active ? styles.active : ''}`}
+        className={classNames(
+          styles.option, {
+            [styles.active]: active,
+            [styles.disabled]: disabled,
+          }
+        )}
         onClick={() => onClick(value)}
       >
         {children}
@@ -28,6 +34,7 @@ Option.propTypes = {
   value: PropTypes.string,
   active: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
   children: PropTypes.any.isRequired,
 };
 
