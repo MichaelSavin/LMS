a84dec0fd2eb8022ec29ce25a4a795de3bdf9109
Evgeny Erohin <erohin.em@gmail.com>
2016-10-25 21:01:27 +0300

    Рефакторинг компонента Option

--- app/components/Course/Content/Editor/Toolbar/Option/index.js
+++ app/components/Course/Content/Editor/Toolbar/Option/index.js
@@ -1,34 +1,26 @@
-import React, {
-  Component,
-  PropTypes,
-} from 'react';
+import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import styles from './styles.css';
 
-class Option extends Component {
-  render() {
-    const {
-      value,
-      active,
-      onClick,
-      children,
-      disabled,
-    } = this.props;
-    return (
-      <div
-        className={classNames(
-          styles.option, {
-            [styles.active]: active,
-            [styles.disabled]: disabled,
-          }
-        )}
-        onClick={() => onClick(value)}
-      >
-        {children}
-      </div>
-    );
-  }
-}
+const Option = ({
+  value,
+  active,
+  onClick,
+  children,
+  disabled,
+}) =>
+  <div
+    className={classNames(
+      styles.option, {
+        [styles.active]: active,
+        [styles.disabled]: disabled,
+      }
+    )}
+    onClick={() => onClick(value)}
+  >
+    {children}
+  </div>;
+
 
 Option.propTypes = {
   value: PropTypes.string,
