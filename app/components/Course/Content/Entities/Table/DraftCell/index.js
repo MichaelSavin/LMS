4afd15938d9ee879a805a93a854bd9346eaa6f47
kortv <2mothy@gmail.com>
2016-12-16 19:12:22 +0300

    [WIP] добавил редактируемую таблицу #82

---
+++ app/components/Course/Content/Entities/Table/DraftCell/index.js
@@ -0,0 +1,195 @@
+import React, {
+  PropTypes,
+} from 'react';
+import {
+  Icon as AntIcon,
+  Form as AntForm,
+  Modal as AntModal,
+  Select as AntSelect,
+  Button as AntButton,
+  Popconfirm as AntPopconfirm,
+} from 'antd';
+import {
+  SortableHandle,
+  SortableElement,
+  SortableContainer,
+} from 'react-sortable-hoc';
+import Preview from '../Preview';
+import styles from './styles.css';
+import { Input as DraftInput } from '../../../Editor';
+
+const DraftCell = ({
+  data,
+  isOpen,
+  addTag,
+  dragTag,
+  removeTag,
+  closeModal,
+  saveSettings,
+  changeTagText,
+  changeTagColor,
+  form: {
+    resetFields,
+    validateFields,
+    getFieldDecorator,
+ },
+}) => {
+  const validateDraft = (_, value, callback) => {
+    if (value.getCurrentContent().getPlainText()) {
+      callback();
+    } else {
+      callback('Это поле не может быть пустым!');
+    }
+  };
+  const validateAndSave = () => {
+    validateFields((err) => {
+      if (!err) { saveSettings(); }
+    });
+  };
+  const resetAndClose = () => {
+    resetFields();
+    closeModal();
+  };
+  return (
+    <span className={styles.tags}>
+      <AntModal
+        onOk={validateAndSave}
+        title={
+          <div className={styles.header}>
+            <span>Тэги</span>
+            <AntButton
+              icon="plus"
+              size="small"
+              type="default"
+              onClick={addTag}
+            >
+              Добавить тэг
+            </AntButton>
+          </div>
+        }
+        okText="Сохранить"
+        visible={isOpen}
+        onCancel={resetAndClose}
+        cancelText="Отмена"
+      >
+        <div className={styles.editor}>
+          <Sortable.List
+            onSortEnd={dragTag}
+            useDragHandle
+          >
+            {data.tags.map((
+              tag,
+              index
+            ) =>
+              <Sortable.Item
+                index={index}
+                key={tag.id}
+              >
+                <div className={styles.tag}>
+                  <Sortable.Handler />
+                  <AntSelect
+                    value={tag.color}
+                    onChange={changeTagColor(index)}
+                    className={styles.color}
+                  >
+                    {['blue',
+                      'red',
+                      'green',
+                    ].map((color) =>
+                      <AntSelect.Option
+                        key={color}
+                        value={color}
+                      >
+                        <div className={styles[color]} />
+                      </AntSelect.Option>
+                    )}
+                  </AntSelect>
+                  <div className={styles.text}>
+                    <AntForm.Item>
+                      {getFieldDecorator(`text.${tag.id}`, {
+                        id: tag.id,
+                        rules: [{ validator: validateDraft }],
+                        initialValue: tag.content,
+                      })(
+                        <DraftInput
+                          size="default"
+                          onChange={changeTagText(index)}
+                          value={tag.content}
+                        />
+                      )}
+                    </AntForm.Item>
+                  </div>
+                  <AntPopconfirm
+                    title="Удалить событие?"
+                    okText="Да"
+                    onConfirm={removeTag(index)}
+                    cancelText="Нет"
+                  >
+                    <AntIcon
+                      type="close"
+                      className={styles.remove}
+                    />
+                  </AntPopconfirm>
+                </div>
+              </Sortable.Item>
+            )}
+          </Sortable.List>
+        </div>
+        <div className={styles.preview}>
+          <div className={styles.title}>
+            <p className={styles.line} />
+            <span className={styles.text}>
+              Предпросмотр
+            </span>
+          </div>
+          <Preview data={data} />
+        </div>
+      </AntModal>
+    </span>
+  );
+};
+
+const Sortable = {
+  List: SortableContainer(
+    (props) => <ul>{props.children}</ul>
+  ),
+  Item: SortableElement(
+    (props) => <li>{props.children}</li>
+  ),
+  Handler: SortableHandle(() =>
+    <AntIcon
+      type="appstore-o"
+      className={styles.drag}
+    />
+  ),
+};
+
+DraftCell.propTypes = {
+  form: PropTypes.shape({
+    resetFields: PropTypes.func.isRequired,
+    validateFields: PropTypes.func.isRequired,
+    getFieldDecorator: PropTypes.func.isRequired,
+  }).isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  addTag: PropTypes.func.isRequired,
+  dragTag: PropTypes.func.isRequired,
+  removeTag: PropTypes.func.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  saveSettings: PropTypes.func.isRequired,
+  changeTagText: PropTypes.func.isRequired,
+  changeTagColor: PropTypes.func.isRequired,
+  data: PropTypes.shape({
+    tags: PropTypes.arrayOf(
+      PropTypes.shape({
+        content: PropTypes.object.isRequired,
+        color: PropTypes.oneOf([
+          'blue',
+          'red',
+          'green',
+        ]).isRequired,
+      }).isRequired,
+    ).isRequired,
+  }).isRequired,
+};
+
+export default DraftCell;
--- app/components/Course/Content/Entities/Table/index.js
+++ app/components/Course/Content/Entities/Table/index.js
@@ -1,81 +1,223 @@
 import React, { Component, PropTypes } from 'react';
-import { Table as AntTable } from 'antd';
-import { isEqual } from 'lodash';
+import {
+  Table as AntTable,
+  Input,
+  Icon, Button, Popconfirm } from 'antd';
+
+// import { isEqual } from 'lodash';
+import { Input as DraftInput } from '../../Editor';
 // import styles from './styles.css';
 
-class Table extends Component {
+// class Table extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = props.content;
+//   constructor(props) {
+//     super(props);
+//     this.state = props.content;
+//   }
+
+//   shouldComponentUpdate(
+//     nextProps,
+//     nextState
+//   ) {
+//     return !isEqual(
+//       this.state,
+//       nextState
+//     );
+//   }
+
+//   render() {
+//     const {
+//       content: {
+//         data,
+//         columns,
+//       },
+//     } = this.props;
+//     return (
+//       <div>
+//         <AntTable
+//           columns={columns}
+//           dataSource={data}
+//           size="small"
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+class EditableCell extends React.Component {
+  state = {
+    value: this.props.value,
+    editable: false,
   }
-
-  shouldComponentUpdate(
-    nextProps,
-    nextState
-  ) {
-    return !isEqual(
-      this.state,
-      nextState
-    );
+  handleChange = (e) => {
+    const value = e.target.value;
+    this.setState({ value });
   }
-
+  check = () => {
+    this.setState({ editable: false });
+    if (this.props.onChange) {
+      this.props.onChange(this.state.value);
+    }
+  }
+  edit = () => {
+    this.setState({ editable: true });
+  }
   render() {
-    const {
-      content: {
-        data,
-        columns,
-      },
-    } = this.props;
-    return (
-      <div>
-        <AntTable
-          columns={columns}
-          dataSource={data}
-          size="small"
-        />
-      </div>
-    );
+    const { value, editable } = this.state;
+    return (<div className="editable-cell">
+      {
+        editable ?
+          <div className="editable-cell-input-wrapper">
+            <Input
+              value={value}
+              onChange={this.handleChange}
+              onPressEnter={this.check}
+            />
+            <Icon
+              type="check"
+              className="editable-cell-icon-check"
+              onClick={this.check}
+            />
+          </div>
+          :
+          <div className="editable-cell-text-wrapper">
+            {value || ' '}
+            <Icon
+              type="edit"
+              className="editable-cell-icon"
+              onClick={this.edit}
+            />
+          </div>
+      }
+    </div>);
   }
 }
 
-Table.propTypes = {
-  entityKey: PropTypes.string.isRequired,
-  content: PropTypes.shape({
-    data: PropTypes.array.isRequired,
-    columns: PropTypes.array.isRequired,
-  }).isRequired,
-};
-
-Table.defaultProps = {
-  content: {
-    columns: [{
-      title: 'Имератор',
+class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.columns = [{
+      title: 'name',
       dataIndex: 'name',
+      width: '30%',
+      render: (text, record, index) => (
+        <EditableCell
+          value={text}
+          onChange={this.onCellChange(index, 'name')}
+          />
+      ),
     }, {
-      title: 'Годы жизни',
-      dataIndex: 'years',
+      title: 'age',
+      dataIndex: 'age',
     }, {
-      title: 'Годы правления',
-      dataIndex: 'reign',
-    }],
-    data: [{
-      key: '1',
-      name: 'Пётр I Алексеевич',
-      years: '1672 — 1725',
-      reign: '1689 — 1725',
+      title: 'address',
+      dataIndex: 'address',
     }, {
-      key: '2',
-      name: 'Александр III Александрович',
-      years: '1845 — 1894',
-      reign: '1881 — 1894',
-    }, {
-      key: '3',
-      name: 'Николай II Александрович',
-      years: '1868 — 1918',
-      reign: '1894 — 1917',
-    }],
-  },
-};
+      title: 'operation',
+      dataIndex: 'operation',
+      render: (text, record, index) => {
+        return (
+          this.state.dataSource.length > 1 ?
+            (
+              <Popconfirm title="Sure to delete?" onConfirm={this.onDelete(index)}>
+                <a href="#">Delete</a>
+              </Popconfirm>
+            ) : null
+        );
+      },
+    }];
 
-export default Table;
+    this.state = {
+      dataSource: [{
+        key: '0',
+        name: 'Edward King 0',
+        age: '32',
+        address: 'London, Park Lane no. 0',
+      }, {
+        key: '1',
+        name: 'Edward King 1',
+        age: '32',
+        address: 'London, Park Lane no. 1',
+      }],
+      count: 2,
+    };
+  }
+  onCellChange = (index, key) => {
+    return (value) => {
+      const dataSource = [...this.state.dataSource];
+      dataSource[index][key] = value;
+      this.setState({ dataSource });
+    };
+  }
+  onDelete = (index) => {
+    return () => {
+      const dataSource = [...this.state.dataSource];
+      dataSource.splice(index, 1);
+      this.setState({ dataSource });
+    };
+  }
+  handleAdd = () => {
+    const { count, dataSource } = this.state;
+    const newData = {
+      key: count,
+      name: `Edward King ${count}`,
+      age: 32,
+      address: `London, Park Lane no. ${count}`,
+    };
+    this.setState({
+      dataSource: [...dataSource, newData],
+      count: count + 1,
+    });
+  }
+  render() {
+    const { dataSource } = this.state;
+    const columns = this.columns;
+    return (<div>
+      <Button className="editable-add-btn" type="ghost" onClick={this.handleAdd}>Add</Button>
+      <AntTable bordered dataSource={dataSource} columns={columns} />
+    </div>);
+  }
+}
+
+// const Comp = ({ text }) => (<h1>{text}</h1>)
+
+// Table.propTypes = {
+//   entityKey: PropTypes.string.isRequired,
+//   content: PropTypes.shape({
+//     data: PropTypes.array.isRequired,
+//     columns: PropTypes.array.isRequired,
+//   }).isRequired,
+// };
+
+// Table.defaultProps = {
+//   content: {
+//     columns: [{
+//       title: <Comp text="Пётр I Алексеевич" />,
+//       dataIndex: 'name',
+//     }, {
+//       title: 'Годы жизни',
+//       dataIndex: 'years',
+//     }, {
+//       title: 'Годы правления',
+//       dataIndex: 'reign',
+//     }],
+//     data: [{
+//       key: '1',
+//       name: <Comp text="Пётр I Алексеевич" />,
+//       years: '1672 — 1725',
+//       reign: '1689 — 1725',
+//     }, {
+//       key: '2',
+//       name: 'Александр III Александрович',
+//       years: '1845 — 1894',
+//       reign: '1881 — 1894',
+//     }, {
+//       key: '3',
+//       name: 'Николай II Александрович',
+//       years: '1868 — 1918',
+//       reign: '1894 — 1917',
+//     }],
+//   },
+// };
+
+export default EditableTable;
