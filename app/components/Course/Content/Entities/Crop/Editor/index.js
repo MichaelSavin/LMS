54b6d5a81448e716b7d0cc713847d90c345429d0
Tim Korshunov <2mothy@gmail.com>
2016-12-20 19:04:34 +0400

    Универсальный загрузчик изображений #113 (#116)
    * [WIP] Добавил компонент Картинки с кропом
    
    * [WIP] коп превью на css
    
    * [wip] фикс превью без выделения #113
    
    * кроп div контейнером
    
    * Перенес кроп на кансвас #113
    
    * [WIP] картинки с загрузкой и кропом #113
    
    Пока достаточным количеством костылей, но по крайней мере работает и
    багов не много.
    
    * готов компонент карточки с кропом #113
    
    Старый компонент сделал CardOrigin чтобы можно было бы видеть что
    поменялось в компоненте Card
    
    * +соотношение сторон и селект при загрузке #113
    
    * добавил обязательное поле для img alt #113
    
    * [WIP] Поменял местами Crop и CardCrop #113
    
    Оригинальный компонент карточки сново лежит в Crop
    Тот что с укропом лежит в CardCrop
    
    * [WIP] удалил лишнее
    
    * Компонент CardCrop переимеенован в Crop #113
    
    * Изменена логика работы компонента Crop #116
    
    Сейчас кропнутые изображения храняться только внутри самого компонента и "вычистляются" при изменении размеров кропа или подмонтировании компонента.
    
    * Поправлена верстка тулбара (компонент Widgets)
    
    * Исправлена ошибка при начальной загруке изображений
    
    Так как загрузка изображений происходит асинхронно - при рендере компонента нужно проверять наличие изображения одновременно в state и storage.

---
+++ app/components/Course/Content/Entities/Crop/Editor/index.js
@@ -0,0 +1,240 @@
+import React, {
+  PropTypes,
+} from 'react';
+import {
+  Icon as AntIcon,
+  Form as AntForm,
+  Input as AntInput,
+  Modal as AntModal,
+  Radio as AntRadio,
+  Upload as AntUpload,
+} from 'antd';
+import Crop from 'react-image-crop/dist/ReactCrop';
+import 'react-image-crop/dist/ReactCrop.css';
+import Preview from '../Preview';
+import styles from './styles.css';
+
+const Editor = ({
+  content: {
+    text,
+    title,
+    image,
+  },
+  isOpen,
+  content,
+  storage,
+  createCrop,
+  closeEditor,
+  uploadImage,
+  removeImage,
+  saveContent,
+  changeContent,
+  form: {
+    resetFields,
+    validateFields,
+    getFieldDecorator,
+ },
+}) => {
+  const validateAndSave = () => {
+    validateFields((err) => {
+      if (!err) { saveContent(); }
+    });
+  };
+  const resetAndClose = () => {
+    resetFields();
+    closeEditor();
+  };
+  return (
+    <AntModal
+      onOk={validateAndSave}
+      title={
+        <div className={styles.header}>
+          <span>Карточка</span>
+        </div>
+      }
+      okText="Сохранить"
+      visible={isOpen}
+      onCancel={resetAndClose}
+      cancelText="Отмена"
+    >
+      <div className={styles.editor}>
+        <div className={styles.content}>
+          <div className={styles.uploader}>
+            {image && storage.image[image.source]
+              ?
+                <div>
+                  <div className={styles.preview}>
+                    <Crop
+                      src={storage.image[image.source]}
+                      crop={image.crop}
+                      onComplete={createCrop}
+                      onAspectRatioChange={createCrop}
+                    />
+                    <AntIcon
+                      type="close"
+                      onClick={removeImage}
+                      className={styles.remove}
+                    />
+                  </div>
+                  {image.crop &&
+                    <div className={styles.aspects}>
+                      Соотношение сторон:
+                      <AntRadio.Group
+                        value={image.crop.aspect}
+                        onChange={changeContent([
+                          'image',
+                          'crop',
+                          'aspect',
+                        ])}
+                      >
+                        {[{ name: '16:9', value: 16 / 9 },
+                          { name: '4:3', value: 4 / 3 },
+                          { name: '1:1', value: 1 },
+                          { name: '3:4', value: 3 / 4 },
+                          { name: '9:16', value: 9 / 16 },
+                          { name: 'Нет', value: false },
+                        ].map(({ name, value }) =>
+                          <AntRadio.Button
+                            key={name}
+                            value={value}
+                          >
+                            {name}
+                          </AntRadio.Button>
+                        )}
+                      </AntRadio.Group>
+                    </div>
+                  }
+                </div>
+              :
+                <AntUpload
+                  type="drag"
+                  accept="image/*"
+                  onChange={uploadImage}
+                  showUploadList={false}
+                >
+                  <div className={styles.upload}>
+                    <div className={styles.icon}>
+                      <AntIcon type="inbox" />
+                    </div>
+                    <div className={styles.hint}>
+                      Нажмите или перетащите файлы для загрузки
+                    </div>
+                  </div>
+                </AntUpload>
+              }
+          </div>
+          {image &&
+            <div className={styles.alt}>
+              <AntForm.Item>
+                {getFieldDecorator('image.text', {
+                  rules: [{
+                    required: true,
+                    message: 'Это поле не может быть пустым!',
+                  }],
+                  initialValue: image.text,
+                })(
+                  <AntInput
+                    size="default"
+                    onChange={changeContent(['image', 'text'])}
+                    placeholder="Альтернативный текст для изображения"
+                  />
+                )}
+              </AntForm.Item>
+            </div>
+          }
+          <div className={styles.title}>
+            <AntInput
+              size="default"
+              value={title}
+              onChange={changeContent(['title'])}
+            />
+          </div>
+          <div className={styles.text}>
+            <AntForm.Item>
+              {getFieldDecorator('text', {
+                rules: [{
+                  required: true,
+                  message: 'Это поле не может быть пустым!',
+                }],
+                initialValue: text,
+              })(
+                <AntInput
+                  rows={4}
+                  type="textarea"
+                  onChange={changeContent(['text'])}
+                />
+              )}
+            </AntForm.Item>
+          </div>
+        </div>
+        <div className={styles.preview}>
+          <div className={styles.title}>
+            <p className={styles.line} />
+            <span
+              className={styles.text}
+            >
+              Предпросмотр
+            </span>
+          </div>
+          <div className={styles.component}>
+            <Preview
+              content={content}
+              storage={storage}
+              placement="editor"
+              dimensions={{ fullscreen: true }}
+            />
+          </div>
+        </div>
+      </div>
+    </AntModal>
+  );
+};
+
+Editor.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  createCrop: PropTypes.func.isRequired,
+  closeEditor: PropTypes.func.isRequired,
+  uploadImage: PropTypes.func.isRequired,
+  removeImage: PropTypes.func.isRequired,
+  saveContent: PropTypes.func.isRequired,
+  changeContent: PropTypes.func.isRequired,
+  content: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    image: PropTypes.shape({
+      source: PropTypes.string.isRequired,
+      text: PropTypes.string,
+      crop: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        pixels: PropTypes.shape({
+          x: PropTypes.number.isRequired,
+          y: PropTypes.number.isRequired,
+          width: PropTypes.number.isRequired,
+          height: PropTypes.number.isRequired,
+        }).isRequired,
+        aspect: PropTypes.oneOf([
+          false,
+          16 / 9,
+          4 / 3,
+          1,
+          3 / 4,
+          9 / 16,
+        ]),
+      }),
+    }),
+  }).isRequired,
+  form: PropTypes.shape({
+    resetFields: PropTypes.func.isRequired,
+    validateFields: PropTypes.func.isRequired,
+    getFieldDecorator: PropTypes.func.isRequired,
+  }).isRequired,
+  storage: PropTypes.shape({
+    crop: PropTypes.object.isRequired,
+    image: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+export default AntForm.create()(Editor);
---
+++ app/components/Course/Content/Entities/Crop/index.js
@@ -0,0 +1,321 @@
+import React, { Component, PropTypes } from 'react';
+import { set, isEqual } from 'lodash/fp';
+import localForage from 'localforage';
+import { Entity } from 'draft-js';
+import Preview from './Preview';
+import Editor from './Editor';
+import './styles.css';
+
+class Card extends Component {
+
+  constructor(props) {
+    super(props);
+    const {
+      content,
+      dimensions,
+    } = props;
+    this.state = {
+      content: {
+        editor: content,
+        component: content,
+      },
+      dimensions,
+      isEditing: false,
+    };
+    this.storage = {
+      image: {},
+      crop: {
+        editor: {},
+        component: {},
+      },
+    };
+  }
+
+  componentDidMount() {
+    const {
+      image,
+    } = this
+      .state
+      .content
+      .component;
+    if (image) {
+      localForage
+        .getItem(image.source)
+        .then((binary) => {
+          const { crop } = this.state
+            .content
+            .component
+            .image;
+          this.storage.image[
+            image.source
+          ] = binary;
+          if (crop) {
+            this.createCrop(
+              crop,
+              crop.pixels,
+              'mountComponent',
+            );
+          } else {
+            this.forceUpdate();
+          }
+        });
+    }
+  }
+
+  shouldComponentUpdate(
+    nextProps,
+    nextState
+  ) {
+    return !isEqual(
+      this.state,
+      nextState
+    );
+  }
+
+  createCrop = (crop, pixelCrop, event = 'changeCrop') => {
+    const { content: { editor: { image } } } = this.state;
+    // Создание уникального канваса для кропа каждого изображения
+    // Нужно для параллельного вычисления нескольких кропов одновременно
+    const canvas = document.createElement('canvas');
+    /* eslint-disable fp/no-mutation */
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
+    const context = canvas.getContext('2d');
+    const imageObj = new Image();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    imageObj.src = this.storage.image[image.source];
+    imageObj.onload = () => {
+    /* eslint-enable fp/no-mutation */
+      context.drawImage(
+        imageObj,
+        pixelCrop.x,
+        pixelCrop.y,
+        pixelCrop.width,
+        pixelCrop.height,
+        0, 0,
+        pixelCrop.width,
+        pixelCrop.height,
+      );
+      const binary = canvas.toDataURL('image/jpeg', 1);
+      this.storage.crop.editor[image.source] = binary;
+      // Срабатывает при подмонтировании компонента
+      if (event === 'mountComponent') {
+        this.storage.crop.component[image.source] = binary;
+        this.forceUpdate();
+      }
+      // Срабатывает при изменении кропа в редакторе
+      if (event === 'changeCrop') {
+        this.setState({
+          content: set([
+            'editor',
+            'image',
+            'crop',
+          ], {
+            ...crop,
+            pixels: pixelCrop,
+          },
+            this.state.content,
+          ),
+        });
+      }
+    };
+  }
+
+  openModal = () => {
+    this.setState({
+      isEditing: true,
+      content: {
+        ...this.state.content,
+        editor: this.state
+          .content
+          .component,
+      },
+    });
+  }
+
+  toggleFullscreen = () => {
+    const dimensions = {
+      ...this.state.dimensions,
+      fullscreen: !this.state
+        .dimensions
+        .fullscreen,
+    };
+    this.setState({ dimensions });
+    Entity.mergeData(
+      this.props.entityKey, {
+        dimensions,
+      }
+    );
+  }
+
+  saveContent = () => {
+    const {
+      content: {
+        editor,
+      },
+    } = this.state;
+    Entity.mergeData(
+      this.props.entityKey, {
+        content: editor,
+      }
+    );
+    // Глубокое копирование Base64 строк изображений
+    this.storage.crop.component = JSON.parse(
+      JSON.stringify(
+        this.storage.crop.editor
+      )
+    );
+    this.setState({
+      content: {
+        ...this.state.content,
+        component: editor,
+      },
+      isEditing: false,
+    });
+  }
+
+  closeEditor = () => {
+    this.setState({
+      isEditing: false,
+    });
+  }
+
+  changeContent = (path) => (event) => {
+    this.setState({
+      content: set([
+        'editor',
+        ...path,
+      ],
+        event.target.value,
+        this.state.content,
+      ),
+    });
+  }
+
+  uploadImage = ({ file }) => {
+    if (file.status === 'error') {
+      const imageName = [
+        file.lastModified,
+        file.size,
+        file.name,
+      ].join('');
+      const reader = new FileReader();
+      reader.readAsDataURL(file.originFileObj);
+      // eslint-disable-next-line fp/no-mutation
+      reader.onloadend = () => {
+        localForage.setItem(
+          imageName,
+          reader.result,
+        ).then(() => {
+          this.storage.image[imageName] = reader.result;
+          this.setState({
+            content: set([
+              'editor',
+              'image',
+            ], {
+              source: imageName,
+            },
+              this.state.content,
+            ),
+          });
+        });
+      };
+    }
+  }
+
+  removeImage = (event) => {
+    event.stopPropagation();
+    this.setState({
+      content: set([
+        'editor',
+        'image',
+      ],
+        undefined,
+        this.state.content,
+      ),
+    });
+  }
+
+  render() {
+    const {
+      content: {
+        editor,
+        component,
+      },
+      isEditing,
+      dimensions,
+    } = this.state;
+    return (
+      <div onDoubleClick={this.openModal}>
+        <Preview
+          content={component}
+          storage={this.storage}
+          placement="component"
+          dimensions={dimensions}
+          toggleFullscreen={this.toggleFullscreen}
+        />
+        <Editor
+          isOpen={isEditing}
+          content={editor}
+          storage={this.storage}
+          createCrop={this.createCrop}
+          closeEditor={this.closeEditor}
+          uploadImage={this.uploadImage}
+          removeImage={this.removeImage}
+          saveContent={this.saveContent}
+          changeContent={this.changeContent}
+        />
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  entityKey: PropTypes.string.isRequired,
+  content: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    image: PropTypes.shape({
+      source: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+      crop: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired,
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        pixels: PropTypes.shape({
+          x: PropTypes.number.isRequired,
+          y: PropTypes.number.isRequired,
+          width: PropTypes.number.isRequired,
+          height: PropTypes.number.isRequired,
+        }).isRequired,
+        aspect: PropTypes.oneOf([
+          false,
+          16 / 9,
+          4 / 3,
+          1,
+          3 / 4,
+          9 / 16,
+        ]),
+      }),
+    }),
+  }).isRequired,
+  dimensions: PropTypes.shape({
+    fullscreen: PropTypes.bool.isRequired,
+    width: PropTypes.number,
+  }).isRequired,
+};
+
+Card.defaultProps = {
+  content: {
+    text: 'Текст',
+    title: 'Заголовок',
+    image: undefined,
+  },
+  dimensions: {
+    fullscreen: false,
+    width: undefined,
+  },
+};
+
+export default Card;
