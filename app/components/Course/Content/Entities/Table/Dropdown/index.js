d5e3b30f8c99605c0877ab5de048720e97e4a48d
kortv <2mothy@gmail.com>
2016-12-27 17:02:39 +0300

    [WIP] пренес overlay в Dropdown #123

--- app/components/Course/Content/Entities/Table/Dropdown/index.js
+++ app/components/Course/Content/Entities/Table/Dropdown/index.js
@@ -5,57 +5,55 @@ import { Menu, Dropdown as AntDropdown, Icon } from 'antd';
 
 import styles from './styles.css';
 
-// Компонент полей выпадающего меню для ячеек таблицы
-// Передается в качестве аргумента overlay в Dropdown
-const RenderMenu = ({
+const Dropdown = ({
   columnKey,
   editTable,
   index,
 }) => (
-  <Menu className={styles.menu}>
-    <Menu.Item key="0">
-      <a onClick={editTable('addColumn', columnKey + 1, index)}>
-        Добавить колонку справа
-      </a>
-    </Menu.Item>
-    <Menu.Item key="1">
-      <a onClick={editTable('addColumn', columnKey, index)}>
-        Добавить колонку слево
-      </a>
-    </Menu.Item>
-    <Menu.Item key="2">
-      <a onClick={editTable('delColumn', columnKey, index)}>
-        Удалить колонку
-      </a>
-    </Menu.Item>
-    <Menu.Divider />
-    {index >= 0 && <Menu.Item key="3">
-      <a onClick={editTable('addRow', columnKey, index)}>
-        Добавить ряд сверху
-      </a>
-    </Menu.Item>}
-    <Menu.Item key="4">
-      <a onClick={editTable('addRow', columnKey, index + 1)}>
-        Добавить ряд снизу
-      </a>
-    </Menu.Item>
-    {index >= 0 && <Menu.Item key="5">
-      <a onClick={editTable('delRow', columnKey, index)}>
-        Удалить ряд
-      </a>
-    </Menu.Item>}
-  </Menu>
-);
-
-const Dropdown = (props) => (
   <div className={styles.dropdown}>
-    <AntDropdown overlay={RenderMenu(props)}>
+    <AntDropdown
+      overlay={
+        <Menu className={styles.menu}>
+          <Menu.Item key="0">
+            <a onClick={editTable('addColumn', columnKey + 1, index)}>
+              Добавить колонку справа
+            </a>
+          </Menu.Item>
+          <Menu.Item key="1">
+            <a onClick={editTable('addColumn', columnKey, index)}>
+              Добавить колонку слево
+            </a>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <a onClick={editTable('delColumn', columnKey, index)}>
+              Удалить колонку
+            </a>
+          </Menu.Item>
+          <Menu.Divider />
+          {index >= 0 && <Menu.Item key="3">
+            <a onClick={editTable('addRow', columnKey, index)}>
+              Добавить ряд сверху
+            </a>
+          </Menu.Item>}
+          <Menu.Item key="4">
+            <a onClick={editTable('addRow', columnKey, index + 1)}>
+              Добавить ряд снизу
+            </a>
+          </Menu.Item>
+          {index >= 0 && <Menu.Item key="5">
+            <a onClick={editTable('delRow', columnKey, index)}>
+              Удалить ряд
+            </a>
+          </Menu.Item>}
+        </Menu>
+      }
+    >
       <Icon type="down" className={styles.icon} />
     </AntDropdown>
   </div>
 );
 
-RenderMenu.propTypes = {
+Dropdown.propTypes = {
   columnKey: PropTypes.number.isRequired,
   editTable: PropTypes.func.isRequired,
   index: PropTypes.number.isRequired,
