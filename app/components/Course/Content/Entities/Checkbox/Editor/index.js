3395ef4f228bea678122fb9af06622ce1ac9facd
Евгений Ерохин <erohin.em@gmail.com>
2017-01-16 20:39:27 +0300

    Исправлена ошибка валидации в компоненте Checkbox

--- app/components/Course/Content/Entities/Checkbox/Editor/index.js
+++ app/components/Course/Content/Entities/Checkbox/Editor/index.js
@@ -667,7 +667,7 @@ const validator = (content) => {
     if (!(variant.hints.length > 0)) { errors[`variants[${variantIndex}].hints`] = `Необходимо добавить подсказки в варианте №${variantIndex + 1}`; }
     if (!(variant.competences.length > 0)) { errors[`variants[${variantIndex}].competences`] = `Необходимо добавить компетенции в варианте №${variantIndex + 1}`; }
     if (!(variant.explanations.length > 0)) { errors[`variants[${variantIndex}].explanations`] = `Необходимо добавить пояснения в варианте №${variantIndex + 1}`; }
-    variant.hints.forEach((hint, hintIndex) => { if (!hint.text) { errors[`variants[${variantIndex}].hints[${hintIndex}].text`] = `Необходимо указать текст подсказки №${optionIndex + 1} в варианте №${variantIndex + 1}`; }});
+    variant.hints.forEach((hint, hintIndex) => { if (!hint.text) { errors[`variants[${variantIndex}].hints[${hintIndex}].text`] = `Необходимо указать текст подсказки №${hintIndex + 1} в варианте №${variantIndex + 1}`; }});
     variant.competences.forEach((competence, competenceIndex) => { if (!competence.text) { errors[`variants[${variantIndex}].competences[${competenceIndex}].text`] = `Необходимо указать текст компетенции №${competenceIndex + 1} в варианте №${variantIndex + 1}`; }});
     variant.explanations.forEach((explanation, explanationIndex) => { if (!explanation.text) { errors[`variants[${variantIndex}].explanations[${explanationIndex}].text`] = `Необходимо указать текст пояснения №${explanationIndex + 1} в варианте №${variantIndex + 1}`; }});
   });
